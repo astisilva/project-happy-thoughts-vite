42748e7a6edd1296202913157867845e9bb95ca0
Fanny <fsb62@hotmail.com>
2023-11-05 03:20:35 +0100

    css

--- src/PostForm.jsx
+++ src/PostForm.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import {React, useState }from 'react'
 import './PostForm.css'
 
 export const PostForm = ({ thoughtValue, newThoughtChange, onSubmitForm, textMessage }) => {
+
+const [remainingCharacters, setRemainingCharacters]=useState(140)
+
+
+const handleTextCharacterChange =(event)=>{
+  const currentLength = event.target.value.length
+  const remaining = 140 - currentLength
+  setRemainingCharacters(remaining)
+  newThoughtChange(event)
+}
+
   return (
     <form onSubmit={onSubmitForm}>
       <h2>What is making you happy right now?</h2>
       <textarea
         value={thoughtValue}
-        onChange={newThoughtChange}
+        onChange={handleTextCharacterChange}
         rows={4}
         cols={70}
-      />
-      <p>{textMessage}</p>
+      /><div className='text'>
+      <p className='text-message'>{textMessage}</p>
+      <p className='remaining-character'>{remainingCharacters}</p>
+      
+      </div>
       <button className='happy-button'>♥ Send happy thought ♥</button>
     </form>
   )
--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './PostList.css'
 
 export const PostList = ({ loading, postList, setPostList, onLike }) => {
+
+  const [likedPostId, setLikedPostId] = useState([])
+
+  const timeDifference = (createdAt) => {
+
+    const currentTime = new Date()
+    const postTime = new Date(createdAt)
+    const timeDifference = Math.abs(currentTime - postTime)
+
+    const minutes = Math.floor(timeDifference / (1000 * 60))
+
+
+    if (minutes < 60) {
+      return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`
+    } else {
+      const hours = Math.floor(minutes / 60);
+      return `about ${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+    }
+
+  }
+
 
   if (loading) {
     return <h1>Loading in progress</h1>
@@ -10,8 +31,11 @@ export const PostList = ({ loading, postList, setPostList, onLike }) => {
 
   const handleLikeClick = (postId) => {
     onLike(postId)
+
+    setLikedPostId((prevLikedPostId) => [...prevLikedPostId, postId])
   }
 
+  const isLiked = (postId) => likedPostId.includes(postId)
 
   return (
 
@@ -19,9 +43,12 @@ export const PostList = ({ loading, postList, setPostList, onLike }) => {
       {postList.map((post) => (
         <div className='list' key={post._id}>
           <p>{post.message}</p>
-          <button className='like-button' onClick={() => { handleLikeClick(post._id) }}>❤</button>
+          <button
+            className={`like-button ${isLiked(post._id) ? 'liked' : ''}`}
+            onClick={() => { handleLikeClick(post._id) }}
+          >❤</button>
           <p>{post.hearts}</p>
-
+          <p>{timeDifference(post.createdAt)}</p>
         </div>
 
       ))}
