89a779f13eab518bed60afa589b1aa9a8fc3961d
Fanny <fsb62@hotmail.com>
2023-11-06 13:46:48 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { Posts } from "./components/Posts";
 import './App.css'
 
 export const App = () => {
-  return <div className="posts">
-  
+  return <div className="posts">  
     <Posts />
   </div>;
 };
